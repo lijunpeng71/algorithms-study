2caed8d46664850a3695a38a6592bd093e86354d
lijunpeng71 <lijunpeng71@163.com>
2020-08-19 07:51:45 +0800

    init

--- algorithms-java-study/src/main/java/com/datastruct/study/graph/GraphDemo.java
+++ algorithms-java-study/src/main/java/com/datastruct/study/graph/GraphDemo.java
@@ -2,8 +2,28 @@ package com.datastruct.study.graph;
 
 public class GraphDemo {
 
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //结点个数
+        int n = 8;
+        String vertexs[] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        GraphObject graphObject = new GraphObject(n);
+        for (String vertex : vertexs) {
+            graphObject.insertVertex(vertex);
+        }
+        //更新边的关系
+        graphObject.insertEdge(0, 1, 1);
+        graphObject.insertEdge(0, 2, 1);
+        graphObject.insertEdge(1, 3, 1);
+        graphObject.insertEdge(1, 4, 1);
+        graphObject.insertEdge(3, 7, 1);
+        graphObject.insertEdge(4, 7, 1);
+        graphObject.insertEdge(2, 5, 1);
+        graphObject.insertEdge(2, 6, 1);
+        graphObject.insertEdge(5, 6, 1);
+        //显示邻接矩阵
+        graphObject.showGraph();
+        graphObject.dfs();
+        System.out.println();
     }
 }
--- algorithms-java-study/src/main/java/com/datastruct/study/graph/GraphObject.java
+++ algorithms-java-study/src/main/java/com/datastruct/study/graph/GraphObject.java
@@ -1,6 +1,7 @@
 package com.datastruct.study.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GraphObject {
@@ -21,12 +22,144 @@ public class GraphObject {
     private int numOfEdges;
 
     /**
+     * 定义数组，记录某个节点是否被访问
+     */
+    private boolean[] visited;
+
+    /**
      * 构造器
      *
      * @param n
      */
     public GraphObject(int n) {
         edges = new int[n][n];
-        vertexList = new ArrayList<String>();
+        vertexList = new ArrayList<>();
+        numOfEdges = 0;
+    }
+
+    /**
+     * 得到第一个邻接节点的下标
+     *
+     * @param index
+     * @return 如果存在就返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接结点的下标获取下一个邻接节点
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    private void dfs(boolean[] visited, int i) {
+        System.out.print(getValueByIndex(i) + "->");
+        visited[i] = true;
+        int w = getFirstNeighbor(i);
+        while (w != -1) {
+            if (!visited[w]) {
+                dfs(visited, w);
+            }
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    /**
+     * 深度优先遍历
+     */
+    public void dfs() {
+        visited = new boolean[vertexList.size()];
+        //遍历所有的结点，进行dfs[回溯]
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!visited[i]) {
+                dfs(visited, i);
+            }
+        }
+    }
+
+    /**
+     * 返回节点的个数
+     *
+     * @return
+     */
+    public int getNumOfVertex() {
+        return vertexList.size();
+    }
+
+    /**
+     * 显示对应的矩阵
+     */
+    public void showGraph() {
+        for (int[] link : edges) {
+            System.out.println(Arrays.toString(link));
+        }
+    }
+
+    /**
+     * 得到边的数目
+     *
+     * @return
+     */
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    /**
+     * 返回结点i(下标)对应的数据
+     *
+     * @param i
+     * @return
+     */
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    /**
+     * 返回v1v2的权值
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    /**
+     * 插入结点
+     *
+     * @param vertex
+     */
+    public void insertVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+
+    /**
+     * 插入边
+     *
+     * @param v1
+     * @param v2
+     * @param weight
+     */
+    public void insertEdge(int v1, int v2, int weight) {
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
     }
 }
