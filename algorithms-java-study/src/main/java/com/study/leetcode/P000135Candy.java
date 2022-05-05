f1b435b6fc7a6c65803de141870ce9ea2b78390e
lijunpeng71 <lijunpeng71@163.com>
2022-05-05 23:14:52 +0800

    init

---
+++ algorithms-java-study/src/main/java/com/study/leetcode/P000135Candy.java
@@ -0,0 +1,30 @@
+package com.study.leetcode;
+
+/**
+ * n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的评分。
+ *
+ * 你需要按照以下要求，给这些孩子分发糖果：
+ *
+ * 每个孩子至少分配到 1 个糖果。
+ * 相邻两个孩子评分更高的孩子会获得更多的糖果。
+ * 请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/candy
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class P000135Candy {
+
+    /**
+     * 分发最少的糖果
+     * @param ratings
+     * @return
+     */
+    public static int candy(int[] ratings) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
