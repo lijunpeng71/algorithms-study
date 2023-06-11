c2a339ba542a2c966b85b41e9b55994f40720254
lijunpeng71 <lijunpeng71@163.com>
2023-06-11 21:59:18 +0800

    init

--- algorithms-java-study/src/main/com/xuelianyong/study/others/Algorithm002.java
+++ algorithms-java-study/src/main/com/xuelianyong/study/others/Algorithm002.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class Algorithm002 {
 
 
-    public static int[] intersection(int arr1[], int arr2[]) {
+    public static int[] intersection(int[] arr1, int[] arr2) {
         if (arr1 == null || arr1.length <= 0 || arr2 == null || arr2.length <= 0) {
             return null;
         }
-        int[] result = new int[arr1.length > arr2.length ? arr2.length : arr1.length];
+        int[] result = new int[Math.min(arr1.length, arr2.length)];
         int i = 0;
         int index1 = 0, index2 = 0;
         while (index1 < arr1.length && index2 < arr2.length) {
